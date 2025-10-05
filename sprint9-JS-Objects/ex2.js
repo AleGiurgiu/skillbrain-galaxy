9c4e9925ca0dcc0d2f5242943df6be3c6f777e7b
Alexandra Giurgiu <topirceanalexandra@yahoo.com>
2025-10-05 14:00:45 +0200

    am terminat tema sprint9

---
+++ sprint9-JS-Objects/ex2.js
@@ -0,0 +1,24 @@
+// Scrieți o funcție care returnează un obiect nou fără proprietățile furnizate
+// Funcția primește 2 parametri ca date de intrare. Unul dintre parametri este obiectul, iar celălalt este proprietatea care nu se dorește să persiste în rezultatul returnat.
+
+// Exemplu 1:
+
+// Input: obiect = { a: 1, b: 2 }, proprietate = “b”
+// Output: { a: 1 }
+
+// Hint 1: https://www.w3schools.com/howto/howto_js_remove_property_object.asp
+// Hint 2: Exista doua moduri de accesare a elementelor prin chei: obiect.cheie sau obiect[”cheie”]
+
+function stergeProprietate(obiectInitial, proprietate) {
+  const obiectNou = { ...obiectInitial };
+  delete obiectNou[proprietate];
+  return obiectNou;
+}
+
+const date = {
+  a: 1,
+  b: 2,
+};
+
+const rezultat = stergeProprietate(date, "a");
+console.log(rezultat);
